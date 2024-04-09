6ec2a7f7f686ecefbef453f8c647b193f678ba50
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-04-08 22:49:17 -0300

    refactor: exchanging img for next image and calling svg with a more suitable name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,15 @@ import {
     HeaderContainerAll, 
     BoxAdd 
 } from "./styleHeader";
-
+import svgBack from '../../../public/images/Return.svg'
+import Image from "next/image";
 interface HeaderProps {
     title: string,
     button: Boolean,
     IconReturn: Boolean,
     PushButton: () => void,
     PushButtonReturn: () => void
+
 }
 
 
@@ -25,8 +27,9 @@ export default function Header({
         <HeaderContainerAll>
             {IconReturn && 
             <BoxRetur>
-                <img onClick={PushButtonReturn} src="/images/Return.svg" alt="Icone Retornar" />
-            </BoxRetur>}
+                <Image onClick={PushButtonReturn} src={svgBack} alt="Icone Retornar" />
+            </BoxRetur>
+            }
             <BoxAdd return={false}>
                 <h5>{title}</h5>
                 {button && <button onClick={PushButton}>Adicionar +</button>}
