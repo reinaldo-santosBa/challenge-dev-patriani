5879e78133f4e479d53702edef664e54cfb8c795
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-04-08 22:48:06 -0300

    chore: added config react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from 'next/app'
 import { GlobalStyle } from '../styles/globalStyle'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <>
+    <QueryClientProvider client={queryClient}>
+
     <Component {...pageProps} />
     <GlobalStyle />
+    </QueryClientProvider>
     </>
   )
 }
