b413e9bb2927d3a7b222b06423ca865b860ade11
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-04-08 22:44:59 -0300

    feat: added new props

--- src/components/buttonFooter/buttonFooter.tsx
+++ src/components/buttonFooter/buttonFooter.tsx
@@ -3,12 +3,13 @@ import { ContainerButton } from "./style";
 interface ButtonFooterProps {
     description: string,
     pushClick: () => void,
+    type?: boolean
 }
 
-export default function ButtonFooter({description, pushClick}: ButtonFooterProps) {
+export default function ButtonFooter({description, pushClick,type}: ButtonFooterProps) {
     return (
         <ContainerButton>
-            <button onClick={pushClick}>{description}</button>
+            <button onClick={pushClick} type={type ? "button" : "submit"}>{description}</button>
         </ContainerButton>
     )
 }
